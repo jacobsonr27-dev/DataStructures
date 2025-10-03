f39626576c74438b6f915028877ebb9e2cec3591
jacobsonr27-dev <jacobsonr27@gfacademy.org>
2025-10-03 13:43:48 -0400

    oct 3

--- 2dboardgame/2dboardgame.py
+++ 2dboardgame/2dboardgame.py
@@ -1,47 +1,68 @@
 class Board:
     def __init__(self, width):
         self.board = []
+        self.player_turn = 1
+        self.winner_found = False
+        self.winner = 'a'
+        self.width = 0
+        self.row = 0
+        self.column = 0
         for i in range(width):
             self.board.append([0]*width)
         for k in range(width):
             print(self.board[k])
     def turn(self, width):
-        player_turn = 1
-        print("It is Player "+ str(player_turn) +"'s turn.")
+        self.width = width
+        print("It is Player "+ str(self.player_turn) +"'s turn.")
         while True:
             while True:
                 try:
-                    row = input("What row would you like to play in? ")
-                    row = int(row)
+                    self.row = input("What row would you like to play in? ")
+                    self.row = int(self.row)
                 except Exception as e:
                     print("Please enter and integer 1 through " + str(width))
                 else:
                     break
             while True:
                 try:
-                    column = input("What column would you like to play in? ")
-                    column = int(column)
+                    self.column = input("What column would you like to play in? ")
+                    self.column = int(self.column)
                 except Exception as e:
                     print("Please enter and integer 1 through " + str(width))
                 else:
                     break
-            print("Player " + str(player_turn) + " is playing at row: " + str(row) + ", column: " + str(column))
-            if self.board[row-1][column-1] != 0:
+            print("Player " + str(self.player_turn) + " is playing at row: " + str(self.row) + ", column: " + str(self.column))
+            if self.board[self.row-1][self.column-1] != 0:
                 print("This spot is already taken, please enter a valid unoccupied space.")
             else:
                 break
-        self.board[row-1][column-1] = player_turn
-        if player_turn == 1:
-            player_turn = 2
-        else:
-            player_turn = 1
-        for k in range(width):
-            print(self.board[k])
+        self.board[self.row-1][self.column-1] = self.player_turn
 
 
+        run_again = True
+        self.check_winner()
+        if  self.winner_found:
+            print("Player " + str(self.winner) + " wins!")
+            exit()
+
+
+
+        if self.player_turn == 1:
+            self.player_turn = 2
+        elif self.player_turn == 2:
+            self.player_turn = 1
+        if run_again:
+            for k in range(self.width):
+                print(self.board[k])
+            self.turn(width)
+
+
+    def check_winner(self):
+        winner_found = False
 
 
 b = Board(10)
+for i in range(2):
+    b.turn(10)
 
-b.turn(10)
 
